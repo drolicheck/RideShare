aa69759c95f5130d4e621cffe1cb3c7b869e312b
tybatt <tbattaglia@csumb.edu>
2014-03-15 09:05:55 -0700

    Sign up page, -first test

--- gen/com/project/ridewithme/R.java
+++ gen/com/project/ridewithme/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -24,11 +29,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f080013;
+        public static final int confirmPassword=0x7f080011;
         public static final int enterEmail=0x7f080004;
         public static final int enterPassword=0x7f080007;
         public static final int loginButton=0x7f080009;
+        public static final int name=0x7f08000c;
         public static final int signUpButton=0x7f08000b;
+        public static final int signupButton=0x7f080012;
         public static final int tableLayout=0x7f080000;
         public static final int tableRow0=0x7f080001;
         public static final int tableRow1=0x7f080002;
@@ -37,23 +45,43 @@ public final class R {
         public static final int tableRow3=0x7f080005;
         public static final int tableRow4=0x7f080006;
         public static final int tableRow5=0x7f080008;
+        public static final int tableRow6=0x7f08000d;
+        public static final int tableRow7=0x7f08000e;
+        public static final int tableRow8=0x7f08000f;
+        public static final int tableRow9=0x7f080010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_my_carpool=0x7f030001;
+        public static final int activity_sign_up=0x7f030002;
+        public static final int fragment_my_carpool=0x7f030003;
+        public static final int fragment_sign_up=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int my_carpool=0x7f070001;
+        public static final int sign_up=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int confirmPassword=0x7f050012;
         public static final int email=0x7f050003;
+        public static final int enterName=0x7f050010;
+        public static final int hello_world=0x7f05000a;
         public static final int loginButton=0x7f050007;
         public static final int loginEmail=0x7f050005;
         public static final int loginPassword=0x7f050006;
+        public static final int look=0x7f05000b;
+        public static final int myCarpool=0x7f05000d;
+        public static final int name=0x7f05000f;
         public static final int password=0x7f050004;
         public static final int signUpButton=0x7f050008;
+        public static final int signupButton=0x7f050011;
+        public static final int start=0x7f05000c;
         public static final int title=0x7f050002;
+        public static final int title_activity_my_carpool=0x7f050009;
+        public static final int title_activity_sign_up=0x7f05000e;
     }
     public static final class style {
         /** 
---
+++ src/com/project/ridewithme/SignUpActivity.java
@@ -0,0 +1,40 @@
+package com.project.ridewithme;
+
+//import com.deitel.favoritetwittersearches.R;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class SignUpActivity extends Activity {
+	
+	private EditText enterUsernames;
+	private EditText enterPassword;
+	private Button signupButton;
+	@Override
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+	
+
+}
